ac782a627cbbcfd3620c5f87815fd6dcefc2f9d6
David Colon <davidcolon07@gmail.com>
2022-11-02 09:43:15 -0700

    useState

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -3,15 +3,17 @@ import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from "./campsitesSlice";
 
 
-const CampsitesList = () => {
+const CampsitesList = ({ setCampsiteId }) => {
     const campsites = selectAllCampsites();
-    
+
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => { // does a "for" loop on campsite array
                 return (
                     // pass each array item `into CampsiteCard as a prop & the value of its "id" key to the "key" prop
-                    <Col md='5' className='m-4' key={campsite.id}>
+                    // onClick, changes the state of the ID to whatever the current item's ID is
+                    // 
+                    <Col onClick={() => setCampsiteId(campsite.id)} md='5' className='m-4' key={campsite.id}>
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
