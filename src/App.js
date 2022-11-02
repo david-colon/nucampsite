30efc870419d1c720b104baf025e02c57baa2591
David Colon <davidcolon07@gmail.com>
2022-11-02 10:15:09 -0700

    header and footer links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { Navbar, NavbarBrand, Container, Row, Col, Collapse, NavbarToggler, Nav, NavItem } from "reactstrap";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import NucampLogo from '../app/assets/img/logo.png'
 
 const Header = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <Navbar dark color='primary' sticky='top' expand='md'>
-            <NavbarBrand href='/'>
-                <img src={NucampLogo} alt='nucamp logo' />
+            <NavbarBrand className='ms-5' href='/'>
+                <img className='float-start' src={NucampLogo} alt='nucamp logo' />
+                <h1 className='mt-1'>NuCamp</h1>
             </NavbarBrand>
-        </Navbar>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse navbar isOpen={menuOpen}>
+                <Nav className="ms-auto" navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'><i className="fa fa-home fa-lg" /> Home</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='directory'><i className="fa fa-home fa-lg" /> Directory</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='about'><i className="fa fa-home fa-lg" /> About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='contact'><i className="fa fa-home fa-lg" /> Contact</NavLink>
+                    </NavItem >
+                </Nav >
+            </Collapse>
+        </Navbar >
     )
 }
 
