6e653529f1c6e51fde528700acfd9d6ed4b08948
David Colon <davidcolon07@gmail.com>
2022-11-17 11:38:45 -0800

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import { Button, FormGroup, Label, Modal, ModalHeader, ModalBody } from 'reactstrap'
 import { ErrorMessage, Formik, Field, Form } from 'formik'
 import validateCommentForm from '../../utils/validateCommentForm'
+import { useDispatch } from 'react-redux'
+import { addComment } from './commentsSlice'
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
 
     const handleSubmit = (values) => {
 
@@ -12,10 +16,12 @@ const CommentForm = ({ campsiteId }) => {
             campsiteId: parseInt(campsiteId),
             rating: values.rating,
             author: values.author,
-            text: values.commentText
+            text: values.commentText,
+            date: new Date(Date.now()).toISOString()
         };
 
         console.log("comment: ", comment);
+        dispatch(addComment(comment));
         setModalOpen(false);
 
     }
